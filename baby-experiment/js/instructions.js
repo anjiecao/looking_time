572d6ba9436f9268a5e83f7d84b5c9286c450b87
GalRaz <galraz@gmail.com>
2021-03-18 20:35:19 -0400

    all the baby changes

--- baby-experiment/js/instructions.js
+++ baby-experiment/js/instructions.js
@@ -1,8 +1,21 @@
 function instructions(timeline){
 
+  timeline.push({
+    type: 'fullscreen',
+    fullscreen_mode: true
+  });
+
+  var audio_check = {
+        type: "audio-keyboard-response",
+        stimulus: 'stimuli/audio/music_intro.wav',
+        prompt: 'Can you hear this?',
+        choices: jsPsych.ALL_KEYS
+      }
+
+  timeline.push(audio_check);
 
 
-for (i = 2; i <= 11; i ++){
+for (i = 2; i <= 9; i ++){
   var intro = {
       type: "html-keyboard-response",
       stimulus: "<p><img src=stimuli/intro/intro" + i + ".jpeg width ='900' height = '500'></p>"
@@ -12,6 +25,14 @@ for (i = 2; i <= 11; i ++){
 
    }
 
+ var ready = {
+     type: "instructions",
+ pages: ["<p> <b> The experiment begins now! </b> </p> <br></br>",
+     ],
+         show_clickable_nav: true}
+
+
+         timeline.push(ready)
 
 return timeline
 }
--- baby-experiment/scripts/generateAudio.m
+++ baby-experiment/scripts/generateAudio.m
@@ -1,16 +1,24 @@
-%% fam clips
+%% intro clip
+[x, Fs] = audioread('/Users/galraz1/Developer/looking_time/baby-experiment/stimuli/audio/music_1m.wav');
 
+audiowrite('/Users/galraz1/Developer/looking_time/baby-experiment/stimuli/audio/music_intro.wav', x/15, Fs);
+
+
+%% fam clips
 [x, Fs] = audioread('/Users/galraz1/Developer/looking_time/baby-experiment/stimuli/audio/full_song.mp3');
+
+% number of chunks
+numChunks = 10;
 
 % get 10 chunks of 6 sec snippets
 snippetLength = Fs*6;
 
 % start points and end points
-startPoints = randi(length(x), [1 10]);
+startPoints = randi(length(x), [1 numChunks]);
 endPoints = startPoints + snippetLength;
 
 % collect clips
-for i = 1:10
+for i = 1:numChunks
     current_clip = x(startPoints(i):endPoints(i));
     
     % break chunk into 5 equal pieces
@@ -23,7 +31,7 @@ for i = 1:10
     current_clip(indices(end-1):indices(end)) = current_clip(indices(end-1):indices(end)) .* linspace(1, 0, indices(end)-indices(end-1)+1);
 
     % write files
-    audiowrite(sprintf('/Users/galraz1/Developer/looking_time/baby-experiment/stimuli/audio/music_%d.wav', i), current_clip, Fs);
+    audiowrite(sprintf('/Users/galraz1/Developer/looking_time/baby-experiment/stimuli/audio/music_%d.wav', i), current_clip/10, Fs);
 end
 
 
@@ -51,5 +59,5 @@ for i = 1:3
     current_clip(indices(end-1):indices(end)) = current_clip(indices(end-1):indices(end)) .* linspace(1, 0, indices(end)-indices(end-1)+1);
 
     % write files
-    audiowrite(sprintf('/Users/galraz1/Developer/looking_time/baby-experiment/stimuli/audio/music_test_%d.wav', i), current_clip, Fs);
+    audiowrite(sprintf('/Users/galraz1/Developer/looking_time/baby-experiment/stimuli/audio/music_test_%d.wav', i), current_clip/10, Fs);
 end
