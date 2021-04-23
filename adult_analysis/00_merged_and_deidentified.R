327e24cf7b3308ec79c65c7bf3e98d8d9f78c464
anjie cao <anjiecao@stanford.edu>
2021-04-23 13:42:50 -0700

    super confusing platform direct comparison

--- adult_analysis/00_merged_and_deidentified.R
+++ adult_analysis/00_merged_and_deidentified.R
@@ -2,12 +2,17 @@ library(here)
 library(tidyverse)
 # currently not deidentifiying!! 
 
-RAW_DATA_DIRECTORY <- here("data/raw_data/")
+#RAW_DATA_DIRECTORY <- here("data/platform_comparison_data/mTurk/raw_data/")
+#MERGED_DATA_PATH <- here("data/platform_comparison_data/mTurk/merged_data/mTurk_merged_data.csv")
+
+RAW_DATA_DIRECTORY <- here("data/platform_comparison_data/prolific/raw_data/")
+MERGED_DATA_PATH <- here("data/platform_comparison_data/prolific/merged_data/merged_data.csv")
+
+
 #RAW_DATA_DIRECTORY <- here("data/raw_data/03_pilot/")
 
 MIN_ROW <- 120  #subject to change 
-MERGED_DATA_PATH <- here("data/merged_data/merged_data.csv")
-
+#MERGED_DATA_PATH <- here("data/merged_data/merged_data.csv")
 
 raw_files <- str_c(RAW_DATA_DIRECTORY, dir(here(RAW_DATA_DIRECTORY), "*.csv"))
 
