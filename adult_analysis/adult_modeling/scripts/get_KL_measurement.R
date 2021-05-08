90381f61cf4b42c8e5a4771670d908c16f914b31
anjie cao <anjiecao@stanford.edu>
2021-05-08 08:59:03 -1000

    still very weird update issue for grid over both epsilon and theta; kl divergence sort of working

---
+++ adult_analysis/adult_modeling/scripts/get_KL_measurement.R
@@ -0,0 +1,77 @@
+
+get_kl_for_creature <- function(distribution_df){
+  
+  all_features <- distribution_df %>% distinct(feature_index) %>% pull()
+  
+  all_feature_kl <- lapply(all_features, 
+         function(x){
+           get_kl_for_feature(x, 
+                              distribution_df)
+           
+           
+         }) %>% 
+    bind_rows()
+  
+  
+  
+  return(all_feature_kl)
+  
+}
+
+
+
+
+get_kl_for_feature <- function(feature = 3, 
+                               distribution_df = all_updates){
+  
+  total_update_number <- length(distribution_df %>% 
+                                  distinct(update_number) %>% 
+                                  pull())
+  
+  all_learning_step_updates <- c()
+  
+  for(update_i in 2:total_update_number){
+    
+    first_update_index <- update_i - 1
+    second_update_index <- update_i 
+    
+    distribtuion_for_feature <- distribution_df %>% 
+      filter(feature_index == feature)
+    
+    second_update <- distribtuion_for_feature %>% 
+      filter(update_number == second_update_index)
+    
+    first_update <- distribtuion_for_feature %>% 
+      filter(update_number == first_update_index)
+    
+    all_thetas <- all_updates %>% distinct(theta) %>% pull()
+    kl <- c() 
+    for(t in all_thetas){
+      
+      second_update_posterior <- second_update %>% 
+        filter(theta == t) %>% 
+        pull(log_posterior)
+      
+      first_update_posterior <- first_update %>% 
+        filter(theta == t)%>% 
+        pull(log_posterior)
+      
+      # because everything is in log
+      kl_for_t <- second_update_posterior + second_update_posterior - first_update_posterior
+      
+      kl <- c(kl, kl_for_t)
+    }
+    
+    current_step_kl <- matrixStats::logSumExp(kl)
+    all_learning_step_updates <- c(all_learning_step_updates, 
+                                   current_step_kl)
+    
+  }
+  
+  learning_updates <- tibble("kl" = all_learning_step_updates) %>% 
+    mutate(update_step = row_number() + 1, 
+           feature_index = feature) 
+  
+  return(learning_updates)
+  
+}
--- adult_analysis/adult_modeling/scripts/grid_approximation.R
+++ adult_analysis/adult_modeling/scripts/grid_approximation.R
@@ -1,4 +1,4 @@
-grid_approximate_creature_with_theta_initial <- function(
+grid_approximate_creature_with_theta <- function(
   grid_theta = seq(0.01, .99, .01), 
   grid_epsilon = seq(0.01, .99, .01), 
   noisy_creature_observation, 
@@ -13,7 +13,7 @@ grid_approximate_creature_with_theta_initial <- function(
     
     lapply(seq(1, feature_number, 1), 
            function(x){
-             first_update_grid_approximate_with_theta(
+             update_grid_approximate_with_theta(
                feature_index = x, 
                thetas = grid_theta, 
                z_bar = noisy_creature_observation[x], 
@@ -31,7 +31,7 @@ grid_approximate_creature_with_theta_initial <- function(
     
     lapply(seq(1, feature_number, 1), 
            function(x){
-             first_update_grid_approximate_with_theta(
+             update_grid_approximate_with_theta(
                feature_index = x, 
                thetas = grid_theta, 
                z_bar = noisy_creature_observation[,x], 
@@ -53,7 +53,7 @@ grid_approximate_creature_with_theta_initial <- function(
 
 }
 
-grid_approximate_creature_with_theta_and_epsilon_initial <- function(
+grid_approximate_creature_with_theta_and_epsilon <- function(
   grid_theta = seq(0.01, .99, .01), 
   grid_epsilon = seq(0.01, .99, .01), 
   noisy_creature_observation, 
@@ -69,7 +69,7 @@ grid_approximate_creature_with_theta_and_epsilon_initial <- function(
     
     lapply(seq(1, feature_number, 1), 
            function(x){
-             first_update_grid_approximate_with_theta_and_epsilon(
+             update_grid_approximate_with_theta_and_epsilon(
                feature_index = x, 
                grid_theta = grid_theta, 
                grid_epsilon = grid_epsilon, 
@@ -90,7 +90,7 @@ grid_approximate_creature_with_theta_and_epsilon_initial <- function(
     
     lapply(seq(1, feature_number, 1), 
            function(x){
-             first_update_grid_approximate_with_theta_and_epsilon(
+             update_grid_approximate_with_theta_and_epsilon(
                feature_index = x, 
                grid_theta = grid_theta, 
                grid_epsilon = grid_epsilon, 
@@ -112,149 +112,12 @@ grid_approximate_creature_with_theta_and_epsilon_initial <- function(
 }
 
 
-grid_approximate_creature_with_theta_continuous <- function(
-  grid_theta = seq(0.01, .99, .01), 
-  grid_epsilon = seq(0.01, .99, .01), 
-  noisy_creature_observation, 
-  updated_posterior_df,
-  alpha_epsilon, 
-  beta_epsilon
-){
-  
-  feature_number = ncol(noisy_creature_observation)
-  
-  
-  lapply(seq(1, feature_number, 1), 
-         function(x){
-           grid_approximation_with_theta(
-             feature_index = x, 
-             grid_theta = grid_theta, 
-             z_bar = noisy_creature_observation[,x], 
-             estimated_posterior_df = filter(updated_posterior_df, feature_index == x),
-             alpha_epsilon = alpha_epsilon, 
-             beta_epsilon = beta_epsilon
-           )
-         }
-  ) %>% 
-    bind_rows()
-  
-  
-  
-}
 
-grid_approximate_creature_with_theta_and_epsilon_continuous <- function(
-  grid_theta = seq(0.01, .99, .01), 
-  grid_epsilon = seq(0.01, .99, .01), 
-  noisy_creature_observation, 
-  updated_posterior_df,
-  alpha_epsilon, 
-  beta_epsilon
-){
-  
-  feature_number = ncol(noisy_creature_observation)
-  
-  lapply(seq(1, feature_number, 1), 
-         function(x){
-           grid_approximation_with_theta_and_epsilon(
-             feature_index = x, 
-             grid_theta = grid_theta, 
-             grid_epsilon = grid_epsilon, 
-             z_bar = noisy_creature_observation[,x], 
-             estimated_posterior_df = filter(updated_posterior_df, feature_index == x),
-             alpha_epsilon = alpha_epsilon, 
-             beta_epsilon = beta_epsilon
-           )
-         }
-  ) %>% 
-    bind_rows()
-  
-  
-}
-
-
-
-
-
-
-
-# single feature 
-## for trial 2 and beyond 
-grid_approximation_with_theta <- function(
-  feature_index = 1, 
-  grid_theta = seq(0.01, .99, .01), 
-  z_bar, 
-  estimated_posterior_df, 
-  alpha_epsilon = alpha_epsilon, 
-  beta_epsilon = beta_epsilon){
-  
-  
-  
-  samps <- tibble("theta" = grid_theta) 
-  
-  
-  samps$unnormalized_log_posterior <- mapply(function(x) update_lp_theta_given_z_after_observation(new_observation = z_bar, 
-                                                                                                      theta = x, 
-                                                                                                      epsilon = epsilon, 
-                                                                                                      updated_posterior = estimated_posterior_df,
-                                                                                                      alpha_epsilon = alpha_epsilon, 
-                                                                                                      beta_epsilon = beta_epsilon), 
-                                              samps$theta)
-  
-  
-  samps$normalized_log_posterior <- samps$unnormalized_log_posterior - matrixStats::logSumExp(samps$unnormalized_log_posterior)
-  samps$feature_index <- feature_index
-  
-  return(samps)
-  
-  
-}
-
-
-## single feature 
-## grid approximation with theta and epsilon 
-grid_approximation_with_theta_and_epsilon <- function(
-  feature_index, 
-  grid_theta = seq(0.01, .99, .01), 
-  grid_epsilon = seq(0.01, .99, .01), 
-  z_bar, 
-  estimated_posterior_df, 
-  alpha_epsilon = alpha_epsilon, 
-  beta_epsilon = beta_epsilon){
-  
-  
-  
-  samps <- expand_grid(theta = grid_theta,
-                       epsilon = grid_epsilon) 
-  
-  
-  samps$unnormalized_log_posterior <- mapply(function(x, y) update_lp_theta_given_z_after_observation(new_observation = z_bar, 
-                                                                                                      theta = x, 
-                                                                                                      epsilon = y, 
-                                                                                                      updated_posterior = estimated_posterior_df,
-                                                                                                      alpha_epsilon = alpha_epsilon, 
-                                                                                                      beta_epsilon = beta_epsilon), 
-                                             samps$theta, 
-                                             samps$epsilon)
-  
-  
-  samps$normalized_log_posterior <- samps$unnormalized_log_posterior - matrixStats::logSumExp(samps$unnormalized_log_posterior)
-  samps <- samps %>% 
-    group_by(theta) %>% 
-    summarise(log_posterior = matrixStats::logSumExp(normalized_log_posterior) + 
-                log(1/length(normalized_log_posterior))) 
-  
-  samps$feature_index <- feature_index
-  
-  
-  return(samps)
-  
-  
-}
 
 # single feature 
 # for trial 1 when beta distribution hasn't been destroyed 
 
-first_update_grid_approximate_with_theta <- function(feature_index = 1, 
+update_grid_approximate_with_theta <- function(feature_index = 1, 
                                      thetas = seq(0.01, .99, .02), 
                                      z_bar, 
                                      epsilon, 
@@ -280,10 +143,10 @@ first_update_grid_approximate_with_theta <- function(feature_index = 1,
   
 }
 
-first_update_grid_approximate_with_theta_and_epsilon <- function(
+update_grid_approximate_with_theta_and_epsilon <- function(
   feature_index,
-  grid_theta = seq(0.01, .99, .02),
-  grid_epsilon = seq(0.01, .99, .02), 
+  grid_theta,
+  grid_epsilon, 
   z_bar, 
   alpha_theta, 
   beta_theta,
@@ -308,26 +171,22 @@ first_update_grid_approximate_with_theta_and_epsilon <- function(
                                              samps$epsilon)
   
   
-  samps$normalized_log_posterior <- samps$unnormalized_log_posterior - 
+
+  
+  samps$log_posterior <- samps$unnormalized_log_posterior - 
     matrixStats::logSumExp(samps$unnormalized_log_posterior)
   
-   # samps <- samps #%>% 
-   #   group_by(theta) %>% 
-   #   
-   #   summarise(
-   #     #unnormalized_log_posterior = matrixStats::logSumExp(unnormalized_log_posterior) + 
-   #     #     log(1/length(unnormalized_log_posterior)), 
-   #     log_posterior = matrixStats::logSumExp(normalized_log_posterior) + 
-   #       log(1/length(normalized_log_posterior))
-   #     
-   #     #unnormalized_log_posterior = matrixStats::logSumExp(unnormalized_log_posterior) + 
-   #    #   log(1/length(unnormalized_log_posterior)), 
-   #    # log_posterior = matrixStats::logSumExp(normalized_log_posterior) + 
-   #     #          log(1/length(normalized_log_posterior))
-   #     ) 
-  samps$feature_index <- feature_index
+  theta_posterior <- samps %>%
+    group_by(theta) %>%
+    summarise(
+      log_posterior = matrixStats::logSumExp(log_posterior) + 
+        log(1/length(log_posterior))) %>%
+    mutate(posterior = exp(log_posterior))
   
-  return(samps)
+  
+  theta_posterior$feature_index <- feature_index
+  
+  return(theta_posterior)
   
 }
   
