61b83da4fcdea4843b0cfa95d13cb9e1bb94243c
GalRaz <galraz@gmail.com>
2021-05-24 19:27:14 -0400

    spent 2h adding observatiosn to update dataframe. i suck at R

---
+++ adult_analysis/adult_modeling/gal_eig_attempt.R
@@ -0,0 +1,3 @@
+library(reshape2)
+
+s_a1b1 <- readRDS(here("adult_modeling/m_res/obs_1_sequential_update.rds"))
--- adult_analysis/adult_modeling/get_eig_entropy_measurement.R
+++
@@ -1,9 +0,0 @@
-get_eig_entropy_for_creature <- function(distribution_df){
-  
-}
-
-
-get_eig_entropy_for_feature <- function(feature, 
-                               distribution_df){
-  
-}
