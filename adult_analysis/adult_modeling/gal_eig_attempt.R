47dba03ac31094b816589fe54d1eef9e79bb66f3
GalRaz <galraz@gmail.com>
2021-05-24 19:39:55 -0400

    pseudocode for eig calculation. taking a break, maybe until tomorrow

--- adult_analysis/adult_modeling/gal_eig_attempt.R
+++ adult_analysis/adult_modeling/gal_eig_attempt.R
@@ -1,3 +1,16 @@
 library(reshape2)
 
-s_a1b1 <- readRDS(here("adult_modeling/m_res/obs_1_sequential_update.rds"))
+s_a1b5 <- readRDS(here("adult_modeling/obs_1_a1b5_sequential_update.rds"))
+
+
+# plan:
+#- add epsilon column
+#- add columns: entropy, kl divergence, alternative entropy, alternative kl divergence
+#- calculate posterior predictive distribution:
+#     - compute p(z|theta, epsilon) --> will give P(z_ij=0) and P(z_ij=1)
+#- get EIG with entropy:  
+# if true z = 1: P(z_ij=0) * (previous - alternative entropy) + P(z_ij=1) * (previous - entropy)
+# else if z = 0: P(z_ij=0) * (previous - entropy) + P(z_ij=1) * (previous - alternative entropy)
+# - get EIG with KL:
+# if true z = 1: P(z_ij=0) * KL(previous dist||alternative dist) + P(z_ij=1) * KL(previous dist||current dist)
+# else if z = 0: P(z_ij=0) * KL(previous dist||current dist) + P(z_ij=1) * KL(previous dist||alternative dist)
