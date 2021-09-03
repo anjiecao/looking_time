dc56587870ebfe81d411c7445ef783321cdf35e5
anjie cao <anjiecao@stanford.edu>
2021-09-02 21:43:06 -0700

    starting building new pipeline, mini reorgs

---
+++ adult_analysis/archive/00_merged_and_deidentified.R
@@ -0,0 +1,34 @@
+library(here)
+library(tidyverse)
+# currently not deidentifiying!! 
+
+#RAW_DATA_DIRECTORY <- here("data/platform_comparison_data/mTurk/raw_data/")
+#MERGED_DATA_PATH <- here("data/platform_comparison_data/mTurk/merged_data/mTurk_merged_data.csv")
+
+#RAW_DATA_DIRECTORY <- here("data/platform_comparison_data/prolific/raw_data/")
+#MERGED_DATA_PATH <- here("data/platform_comparison_data/prolific/merged_data/merged_data.csv")
+
+
+RAW_DATA_DIR <- here("data/00_raw_data/")
+merged_data_path <- here("data/01_merged_data/merged_data.csv")
+
+
+raw_files <- str_c(RAW_DATA_DIRECTORY, dir(here(RAW_DATA_DIRECTORY), "*.csv"))
+
+# count how many rows in each file 
+RAW_count <- map_df(raw_files, function(file) {
+  d <- read_csv(file) %>% 
+    count() %>% 
+    mutate(
+      file_name = file 
+    )
+})   
+
+
+RAW_DATA <- map_df((RAW_count %>% filter(n > MIN_ROW))$file_name,
+                   function(file){
+                     d <- read_csv(file)
+                   })
+
+
+write_csv(RAW_DATA, MERGED_DATA_PATH)
---
+++ adult_analysis/study_1/00_merged_and_deidentified.R
@@ -0,0 +1,46 @@
+library(here)
+library(tidyverse)
+# currently not deidentifiying!! 
+
+#RAW_DATA_DIRECTORY <- here("data/platform_comparison_data/mTurk/raw_data/")
+#MERGED_DATA_PATH <- here("data/platform_comparison_data/mTurk/merged_data/mTurk_merged_data.csv")
+
+#RAW_DATA_DIRECTORY <- here("data/platform_comparison_data/prolific/raw_data/")
+#MERGED_DATA_PATH <- here("data/platform_comparison_data/prolific/merged_data/merged_data.csv")
+
+
+RAW_DATA_DIRECTORY <- here("data/00_raw_data/")
+
+MIN_ROW <- 100  #subject to change 
+MERGED_DATA_PATH <- here("data/01_merged_data/merged_data.csv")
+
+merge_and_deidentify_data <- function(raw_data_directory, 
+                                      merged_data_path, 
+                                      min_row){
+  
+  raw_files <- str_c(raw_data_directory, dir(here(raw_data_directory), "*.csv"))
+  
+  # count how many rows in each file 
+  RAW_count <- map_df(raw_files, function(file) {
+    d <- read_csv(file) %>% 
+      count() %>% 
+      mutate(
+        file_name = file 
+      )
+  })   
+  
+  
+  RAW_DATA <- map_df((RAW_count %>% filter(n > min_row))$file_name,
+                     function(file){
+                       d <- read_csv(file)
+                     })
+  
+  
+  write_csv(RAW_DATA, merged_data_path)
+  
+  
+  
+}
+
+
+merge_and_deidentify_data(RAW_DATA_DIRECTORY, MERGED_DATA_PATH, MIN_ROW)
