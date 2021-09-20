b6d1cf794c98dfbe6e7e5355cc288be1c2177349
anjie cao <anjiecao@stanford.edu>
2021-09-19 21:11:43 -0700

    added instruction

--- preschoooler-experiment/js/02_task_instruction_and_practice.js
+++ preschoooler-experiment/js/02_task_instruction_and_practice.js
@@ -138,7 +138,7 @@ var great_job = {
             exposure_type: "self_paced"
         }
 
-        var practice_block = {
+        var practice_block_1 = {
           timeline: [
             {
                 type: 'html-keyboard-response',
@@ -178,15 +178,15 @@ var great_job = {
                 exposure_type: exposure_type
             }
             ],
-            timeline_variables: [{stimuli: "images/practice/instructions_example_spore2.gif", 
+            timeline_variables: [{stimuli: "images/practice/memory_practice_1.gif", 
                                   stim_type: "background", 
                                   exposure_type: "self_paced", 
                                   first_trial: true}, 
-                                  {stimuli: "images/practice/instructions_example_spore2.gif", 
+                                  {stimuli: "images/practice/memory_practice_1.gif", 
                                   stim_type: "background", 
                                   exposure_type: "self_paced", 
                                   first_trial: false}, 
-                                  {stimuli: "images/practice/instructions_example_spore2.gif", 
+                                  {stimuli: "images/practice/memory_practice_1.gif", 
                                   stim_type: "background", 
                                   exposure_type: "self_paced", 
                                   first_trial: false}
@@ -195,30 +195,81 @@ var great_job = {
 
         
 
-        var practice_memory = {
-                  type: "survey-multi-choice",
-                  preamble: '<p><img src= ' +  "images/practice/instructions_example_spore2.gif" + ' width ="400" height = "400"</p>',
-                  questions: [
-                    {prompt: 'Did you look at this monster before?', // actually we can request control from the participants 
-                    options: ["Yes", "No"], 
-                    required: false, 
-                    horizontal: true},
-                  ],
-                  data: {stimulus_type: 'memory_test_practice',
-                        memory_question_stimuli: "images/practice/instructions_example_spore2.gif", 
-                        memory_block_index: 0 },
-          }
 
-
-          var practice_memory = {
+          var practice_memory_1 = {
             type: 'html-button-response', 
             stimulus: '',
-            choices: ["right", "wrong"], 
+            choices: ["right"], 
             margin_horizontal: 20,
-            button_html: ['<p><img src= ' +   "images/practice/instructions_example_spore2.gif" + ' width ="400" height = "400"</p>', 
-                        '<p><img src= ' +   "images/practice/practice_memory.gif"  + ' width ="400" height = "400"</p>'
+            button_html: ['<p><img src= ' +   "images/practice/memory_practice_1.gif" + ' width ="400" height = "400"</p>'
                         ]
-    }
+         }
+
+
+
+         var practice_block_2 = {
+          timeline: [
+            {
+                type: 'html-keyboard-response',
+                stimulus: '<p><img src= images/blank.png width ="600" height = "600" style="position:fixed;top:50%; left:50%; transform: translate(-50%, -50%); border:5px solid black">',
+                trial_duration: function(){
+                    var random_duration = 800 + 500 * Math.random() 
+                    return random_duration  } ,
+                choices: jsPsych.NO_KEYS
+            },
+            {
+                type: 'stimuli-presentation',
+                frame_animation: function(){
+                    var html =  '<p><img src= images/blank.png width ="600" height = "600" style="position:fixed; top:50%; transform: translate(-50%, -50%);left:50%;border:5px solid black">'
+                   
+                    return html;
+                },
+                
+              stimuli_animation: function(){
+                    function getRandomInt(max) {
+                        return Math.floor(Math.random() * max);
+                        }
+                    var width_height = parseFloat(Math.floor(Math.random() * 10) + 250);
+                    var html = "<p><img src="+jsPsych.timelineVariable('stimuli', true)+" width ='" + width_height + "' height = '" + width_height + "' style='position:relative;top:" + Math.random() * (80 - 10) + 10 + "%;left:" +  Math.random() * (80 - 10) + 10  +  "%'></p>"
+                    return html
+                },
+           
+                block_deviant: block_information.deviant_stimuli,
+                block_background: block_information.background_stimuli,
+                key_response: [32],
+                first_trial: function(){
+                    return jsPsych.timelineVariable('first_trial', true)
+                }, 
+                minimum_viewing_duration: 500, // in non-first trial and self-paced first trial, 
+                forced_short_viewing_duration: 10 * (getRandomInt(forced_short_viewing_duration_base, 100)), //50 ~ 1000 with 10 interval random
+                forced_long_viewing_duration: forced_long_viewing_duration,
+                response_ends_trial: true,
+                exposure_type: exposure_type
+            }
+            ],
+            timeline_variables: [{stimuli: "images/practice/memory_practice_2.gif", 
+                                  stim_type: "background", 
+                                  exposure_type: "self_paced", 
+                                  first_trial: true}, 
+                                  {stimuli: "images/practice/memory_practice_2.gif", 
+                                  stim_type: "background", 
+                                  exposure_type: "self_paced", 
+                                  first_trial: false}, 
+                                  {stimuli: "images/practice/memory_practice_2.gif", 
+                                  stim_type: "background", 
+                                  exposure_type: "self_paced", 
+                                  first_trial: false}
+                                 ]
+            }
+
+    var practice_memory_2 = {
+      type: 'html-button-response', 
+      stimulus: '',
+      choices: ["right"], 
+      margin_horizontal: 20,
+      button_html: ['<p><img src= ' +   "images/practice/memory_practice_2_novel.gif" + ' width ="400" height = "400"</p>'
+                  ]
+      }
 
         var intro3 = {
             type: 'stimuli-presentation',
@@ -245,8 +296,10 @@ var instruction_and_practice = [
             first_example, 
             great_job, 
             look_carefully, 
-            practice_block, 
-            practice_memory, 
+            practice_block_1, 
+            practice_memory_1,
+            practice_block_2, 
+            practice_memory_2, 
             sticker_books, 
             intro3
                           ]
