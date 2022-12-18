0a4555302444cc3a4a6608f3b09ea5b37ab0ef8e
anjie cao <anjiecao@stanford.edu>
2022-12-17 21:22:29 -0800

    add filler task

--- adult_experiment/study_3/js/helper/get_blocks.js
+++ adult_experiment/study_3/js/helper/get_blocks.js
@@ -12,9 +12,10 @@ function generate_all_blocks(all_blocks_info){
 
 function generate_block(block_info){
 
+
     if(block_info.block_type == "background_block"){
         background_html = generate_html_string_for_stimulus(block_info.background_type, block_info.background_stimulus)
-
+        filler_trial_html = generate_html_string_for_filler_task(block_info.background_type, block_info.background_stimulus)
         // generate all trials
         block = []
         for (var i = 0; i < block_info.trial_number; i++){
@@ -27,10 +28,37 @@ function generate_block(block_info){
               block.push(trial)
         }
 
+        var filler_trial = {
+            type: jsPsychSurveyLikert,
+            preamble: filler_trial_html,
+            questions: [
+              {
+                prompt: "How curious are you about this animation?", 
+                labels: ["Not at all curious", 
+                "A little curious", 
+                "Somewhat curious", 
+                "Pretty curious", "Very Curious"]
+              }
+            ],
+            data: {
+                rating_type: "background"
+            }
+          }
+        
+        block.push(filler_trial)
+        console.log(block)
+
   
     }else{
         background_html = generate_html_string_for_stimulus(block_info.background_type, block_info.background_stimulus)
         deviant_html =  generate_html_string_for_stimulus(block_info.deviant_type, block_info.deviant_stimulus)
+
+        rating_stimuli_type = getRandomSubarray(["background", "deviant"],1)[0]
+        if (rating_stimuli_type == "background"){
+            filler_trial_html = generate_html_string_for_filler_task(block_info.background_type, block_info.background_stimulus)
+        }else{
+            filler_trial_html = generate_html_string_for_filler_task(block_info.deviant_type, block_info.deviant_stimulus)
+        }
 
         // generate all background
         block = []
@@ -51,6 +79,26 @@ function generate_block(block_info){
             data: block_info
         }
         block.push(trial)
+
+        // add filler task 
+        var filler_trial = {
+            type: jsPsychSurveyLikert,
+            preamble:  filler_trial_html,
+            questions: [
+              {
+                prompt: "How curious are you about this animation?", 
+                labels: ["Not at all curious", 
+                "A little curious", 
+                "Somewhat curious", 
+                "Pretty curious", "Very Curious"]
+              }
+            ],
+            data: {
+                rating_type: rating_stimuli_type
+            }
+          }
+          block.push(filler_trial)
+          
 
   
     }
@@ -83,6 +131,30 @@ function generate_html_string_for_stimulus(stimulus_type, stimulus_string){
     return(s)
  }
  
+ 
+
+function generate_html_string_for_filler_task(stimulus_type, stimulus_string){
+   
+
+    var top_position = 20
+    var left_postion = 46
+    
+    
+    if(stimulus_type.includes("pair")){
+        s = '<img src="' + stimulus_string + '" class="coveredImage test" style = "width:100px; height:100px;position:fixed; top:' + top_position + '%;\
+            transform: translate(-50%, -50%);left:' + (left_postion-3) + '%"></img>'+
+            '<img src="' + stimulus_string + '" class="coveredImage test" style = "width:100px; height:100px;position:fixed; top:' + top_position  + '%;\
+            transform: translate(-50%, -50%);left:' + (left_postion + 3) + '%">'
+    }else{
+        s = '<img src="' + stimulus_string + '" class="coveredImage test" style = "width:100px; height:100px;position:fixed; top:' + top_position + '%;\
+        transform: translate(-50%, -50%);left:' + 46 + '%"></img>'
+        
+
+
+    }
+
+    return(s)
+ }
  
 
 
