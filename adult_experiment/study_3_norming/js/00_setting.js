a621b67f92237e91d01e20deb944cfb9eb41bead
anjie cao <anjiecao@stanford.edu>
2023-02-13 11:12:53 -0800

    mock up similarity single trial structure

--- adult_experiment/study_3_norming/js/00_setting.js
+++ adult_experiment/study_3_norming/js/00_setting.js
@@ -5,6 +5,8 @@ all_stimuli_info = get_all_stimuli_info(all_stimuli)
 shuffleArray(all_stimuli_info)
 
 all_complexity_rating = get_all_complexity_rating(all_stimuli_info)
+similarity_trial = generate_stimilarity_rating_trial(all_stimuli_info[0], all_stimuli_info[1], all_stimuli_info[2])
+
 
 
 
